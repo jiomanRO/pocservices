d7e75b8930a0ce9791ccf422258a85b848e9a7fa
jiomanRO <jioman@gmail.com>
2020-11-18 16:01:08 +0200

    Add health endpoint.

--- frontservice/src/main/java/com/orange/john/frontservice/controllers/SimpleHealthController.java
+++ frontservice/src/main/java/com/orange/john/frontservice/controllers/SimpleHealthController.java
@@ -1,5 +1,6 @@
 package com.orange.john.frontservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class SimpleHealthController {
     @RequestMapping("/simpleHealth")
     public String simpleHealth() {
         return "ALLOK";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "HEALTHY";
     }
 }
